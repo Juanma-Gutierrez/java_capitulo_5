2e9fea1c20609fe3edee70c6368d59c7b9f4e55b
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-17 17:06:56 +0200

    feat: Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,18 @@
+/**
+ * Exercise: 5
+ *
+ * @version: 17/10/2022 17:05
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int i;
+        i = 320;
+        while (i >= 160) {
+            System.out.print(i + ", ");
+            i -= 20;
+        }
+    }
+}
