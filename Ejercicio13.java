e143230ccca3122a84b6a628a49ee2486742fef7
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-17 18:30:55 +0200

    feat: Ejercicio13

---
+++ Ejercicio13.java
@@ -0,0 +1,34 @@
+
+/**
+ * Exercise: 13
+ *
+ * @version: 17/10/2022 18:26
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pos;
+        int neg;
+        int num;
+
+        pos = 0;
+        neg = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Introduce un número: ");
+            num = sc.nextInt();
+            if (num > 0)
+                pos++;
+            else if (num < 0)
+                neg++;
+        }
+
+        System.out.println("Números positivos introducidos: " + pos);
+        System.out.println("Números negativos introducidos: " + neg);
+        sc.close();
+    }
+}
