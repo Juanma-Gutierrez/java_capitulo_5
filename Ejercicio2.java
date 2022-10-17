805604721271a883d54a05f8e05168f733cbcb21
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-17 17:01:31 +0200

    feat: Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,18 @@
+/**
+ * Exercise: 2
+ *
+ * @version: 17/10/2022 16:57
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int i;
+        i = 0;
+        while (i <= 100) {
+            System.out.print(i + ", ");
+            i += 5;
+        }
+    }
+}
