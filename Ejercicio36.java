2eeb90bc1fa1c1d4e92d16dc20280636a5971cdb
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-19 18:26:46 +0200

    feat: Ejercicio36

---
+++ Ejercicio36.java
@@ -0,0 +1,47 @@
+
+/**
+ * Exercise: 36
+ *
+ * @version: 19/10/2022 18:15
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio36 {
+    public static void main(String[] args) {
+        // Vars definition
+        int num;
+        int longNum;
+        boolean cap;
+        int neg;
+
+        // Vars init
+        cap = true;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.print("Introduce un número entero: ");
+        num = sc.nextInt();
+        longNum = Integer.toString(num).length();
+        neg = (num < 0) ? 1 : 0;
+
+        // Process
+        for (int i = 0; i < longNum / 2; i++) {
+            if (Integer.toString(num).charAt(i + neg) != Integer.toString(num).charAt(longNum - i - 1)) {
+                cap = false;
+            }
+        }
+
+        // Output data
+        System.out.print((cap) ? "El " + num + " es capicúa " : "El " + num + " no es capicúa");
+        if (neg == 1)
+            System.out.print("y de signo negativo");
+
+        sc.close();
+    }
+
+}
