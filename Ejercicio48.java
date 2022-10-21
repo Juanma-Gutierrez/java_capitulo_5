f8cfeab92e97b1d7abba6025e5ea536977785cdd
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-21 19:04:02 +0200

    feat: Ejercicio49

--- Ejercicio48.java
+++ Ejercicio48.java
@@ -12,119 +12,45 @@ import java.util.Scanner;
 public class Ejercicio48 {
     public static void main(String[] args) {
         // Var declarations
-        int zero;
-        int one;
-        int two;
-        int three;
-        int four;
-        int five;
-        int six;
-        int seven;
-        int eight;
-        int nine;
-        int num;
-
-        // Var init
-        zero = 0;
-        one = 0;
-        two = 0;
-        three = 0;
-        four = 0;
-        five = 0;
-        six = 0;
-        seven = 0;
-        eight = 0;
-        nine = 0;
+        long num;
+        long aux;
+        boolean find;
 
         // Scanner class
         Scanner sc = new Scanner(System.in);
 
         // Input data
         System.out.print("Introduzca un número entero: ");
-        num = sc.nextInt();
+        num = sc.nextLong();
 
         // Process
-        do {
-            switch (num % 10) {
-                case 0:
-                    zero++;
-                    break;
-                case 1:
-                    one++;
-                    break;
-                case 2:
-                    two++;
-                    break;
-                case 3:
-                    three++;
-                    break;
-                case 4:
-                    four++;
-                    break;
-                case 5:
-                    five++;
-                    break;
-                case 6:
-                    six++;
-                    break;
-                case 7:
-                    seven++;
-                    break;
-                case 8:
-                    eight++;
-                    break;
-                case 9:
-                    nine++;
-                    break;
+        // Find numbers
+        System.out.print("Dígitos que aparecen: ");
+        for (int i = 0; i < 10; i++) {
+            find = false;
+            aux = num;
+            while (aux > 0) {
+                if (aux % 10 == i)
+                    find = true;
+                aux /= 10;
             }
-            num /= 10;
-        } while (num > 0);
+            if (find)
+                System.out.print(i + " ");
+        }
 
-        // Output data
-        System.out.print("Dígitos que aparecen en el número: ");
-        if (zero > 0)
-            System.out.print("0 ");
-        if (one > 0)
-            System.out.print("1 ");
-        if (two > 0)
-            System.out.print("2 ");
-        if (three > 0)
-            System.out.print("3 ");
-        if (four > 0)
-            System.out.print("4 ");
-        if (five > 0)
-            System.out.print("5 ");
-        if (six > 0)
-            System.out.print("6 ");
-        if (seven > 0)
-            System.out.print("7 ");
-        if (eight > 0)
-            System.out.print("8 ");
-        if (nine > 0)
-            System.out.print("9 ");
-        System.out.println();
-
-        System.out.print("Dígitos que aparecen en el número: ");
-        if (zero == 0)
-            System.out.print("0 ");
-        if (one == 0)
-            System.out.print("1 ");
-        if (two == 0)
-            System.out.print("2 ");
-        if (three == 0)
-            System.out.print("3 ");
-        if (four == 0)
-            System.out.print("4 ");
-        if (five == 0)
-            System.out.print("5 ");
-        if (six == 0)
-            System.out.print("6 ");
-        if (seven == 0)
-            System.out.print("7 ");
-        if (eight == 0)
-            System.out.print("8 ");
-        if (nine == 0)
-            System.out.print("9 ");
+        // Find numbers doesn't appears
+        System.out.print("\nDígitos que noaparecen: ");
+        for (int i = 0; i < 10; i++) {
+            find = false;
+            aux = num;
+            while (aux > 0) {
+                if (aux % 10 == i)
+                    find = true;
+                aux /= 10;
+            }
+            if (!find)
+                System.out.print(i + " ");
+        }
 
         sc.close();
     }
---
+++ Ejercicio49.java
@@ -0,0 +1,64 @@
+
+/**
+ * Exercise: 49
+ *
+ * @version: 21/10/2022 18:22
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio49 {
+    public static void main(String[] args) {
+        // Var declarations
+        int sum;
+        int count;
+        int min;
+        int max;
+        int num;
+        boolean prime;
+
+        // Var init
+        sum = 0;
+        count = 0;
+        min = 2147483647;
+        max = 0;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.println(
+                "Por favor, vaya introduciendo números enteros positivos. Para terminar, introduzca un número primo:");
+
+        // Process
+        do {
+            num = sc.nextInt();
+
+            // CheckPrime
+            prime = true;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0)
+                    prime = false;
+            }
+            if (!prime) {
+                if (num > max)
+                    max = num;
+                if (num < min)
+                    min = num;
+                sum += num;
+                count++;
+            }
+
+        } while (!prime);
+
+        // Output data
+        System.out.println("Ha introducido " + count + " números no primos.");
+        System.out.println("Máximo: " + max);
+        System.out.println("Mínimo: " + min);
+        System.out.printf("Media: %.2f", (float) sum / count);
+
+        sc.close();
+    }
+}
