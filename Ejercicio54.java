8bac5981e98f69c80c78ddf64b49734c75a6a0f3
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-22 11:40:40 +0200

    feat: Ejercicio54

---
+++ Ejercicio54.java
@@ -0,0 +1,36 @@
+
+/**
+ * Exercise: 54
+ *
+ * @version: 22/10/2022 11:38
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio54 {
+    public static void main(String[] args) {
+        // Var declarations
+        int height;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.print("Introduzca la altura de la figura: ");
+        height = sc.nextInt();
+
+        // Process
+        for (int i = height; i > 0; i--) {
+            for (int j = 0; j < i; j++)
+                if (i == height || i == 1 || j == i - 1 || j == 0)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
