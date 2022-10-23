f6442e72369f2b3c5444b33496057ea3e03f5b2b
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-23 11:10:25 +0200

    feat: Ejercicio68

---
+++ Ejercicio68.java
@@ -0,0 +1,57 @@
+
+/**
+ * Exercise: 68
+ *
+ * @version: 23/10/2022 11:01
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio68 {
+    public static long rotateNum(long num) {
+        long rotateNum;
+        rotateNum = 0;
+        while (num > 0) {
+            rotateNum *= 10;
+            rotateNum += num % 10;
+            num /= 10;
+        }
+        return rotateNum;
+    }
+
+    public static void main(String[] args) {
+        // Var declarations
+        long num;
+        long revNum;
+        long dislocNum;
+
+        // Var init
+        dislocNum = 0;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.print("Por favor, introduzca un número: ");
+        num = sc.nextLong();
+
+        // Process
+        revNum = rotateNum(num);
+        do {
+            if (revNum % 10 % 2 == 0) {
+                dislocNum *= 10;
+                dislocNum += revNum % 10 + 1;
+            } else {
+                dislocNum *= 10;
+                dislocNum += revNum % 10 - 1;
+            }
+            revNum /= 10;
+
+        } while (revNum > 0);
+
+        System.out.print("Dislocando el " + num + " sale el " + dislocNum);
+        sc.close();
+    }
+}
